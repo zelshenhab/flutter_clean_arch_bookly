5646c5dc113ca106fcf9b4d93fc1a19e39f343aa
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2024-08-14 20:45:34 +0300

    fetch newset books use case

--- lib/Features/home/domain/use_cases/fetch_newset_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_newset_books_use_case.dart
@@ -2,13 +2,16 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_bookly/Features/home/domain/entities/book_entitiy.dart';
 import 'package:flutter_bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:flutter_bookly/core/errors/failures.dart';
+import 'package:flutter_bookly/core/use_cases/use_cases.dart';
 
-class FetchNewsetBooksUseCase {
+class FetchNewsetBooksUseCase extends UseCase<List<BookEntitiy>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchNewsetBooksUseCase(this.homeRepo);
 
-  Future<List<Either<Failure, List<BookEntitiy>>>> fetchNewestBooks() {
-    return homeRepo.fetchNewestBooks();
+  @override
+  Future<List<Either<Failure, List<BookEntitiy>>>> call(
+      [NoParam? param]) async {
+    return await homeRepo.fetchNewestBooks();
   }
 }
