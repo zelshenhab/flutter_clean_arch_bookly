3d50d713571bc636bf111ad70350c071bfe658ea
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2024-08-20 21:24:33 +0300

    implement cubit fetch featured books

--- lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,18 +1,16 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bookly/Features/home/data/models/book_model/book_model.dart';
-import 'package:flutter_bookly/Features/home/data/repos/home_repo.dart';
 import 'package:flutter_bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:flutter_bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
-
-  final HomeRepo homeRepo;
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase featuredBooksUseCase;
 
   Future<void> fetchFeaturedBooks() async {
     emit(FeaturedBooksLoading());
-    var result = await homeRepo.fetchFeaturedBooks();
+    var result = await featuredBooksUseCase.call();
     result.fold((failure) {
       emit(FeaturedBooksFailure(failure.message));
     }, (books) {
@@ -20,3 +18,14 @@ class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
     });
   }
 }
+
+
+//var result = await homeRepo.fetchFeaturedBooks();
+    // result.fold(
+    //   (failure) {
+    //     emit(FeaturedBooksFailure(failure.message));
+    //   },
+    //   (books) {
+    //     emit(FeaturedBooksSuccess(books));
+    //   },
+    // );
