300bf1584dbcc41bb7e703d6d3e172918fc8b087
Zeyad Elshenhab <z.elshenhab2012@gmail.com>
2024-08-20 23:12:09 +0300

    create newest books cubit

--- lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,18 +1,15 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bookly/Features/home/data/models/book_model/book_model.dart';
-import 'package:flutter_bookly/Features/home/data/repos/home_repo.dart';
 import 'package:flutter_bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:flutter_bookly/Features/home/domain/use_cases/fetch_newset_books_use_case.dart';
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
-
-  final HomeRepo homeRepo;
-
-  Future<void> fetchNewsetBooks() async {
+  NewsetBooksCubit(this.fetchNewsetBooksUseCase) : super(NewsetBooksInitial());
+  final FetchNewsetBooksUseCase fetchNewsetBooksUseCase;
+  Future<void> fetchNewestBooks() async {
     emit(NewsetBooksLoading());
-    var result = await homeRepo.fetchNewsetBooks();
+    var result = await fetchNewsetBooksUseCase.call();
     result.fold((failure) {
       emit(NewsetBooksFailure(failure.message));
     }, (books) {
